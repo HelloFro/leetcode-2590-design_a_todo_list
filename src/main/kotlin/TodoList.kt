e42d5c319ed9fb220168d176fa67ef56e4704833
Fro Warner <hellofroenn@gmail.com>
2025-04-15 13:20:58 -0700

    Implement addTask()

--- src/main/kotlin/TodoList.kt
+++ src/main/kotlin/TodoList.kt
@@ -1,8 +1,30 @@
 package dev.hellofro
 
 class TodoList {
+    private val _userTasks = mutableMapOf<Int, MutableMap<Int, Task>>().withDefault { mutableMapOf() }
+    private val _tagIndex = mutableMapOf<String, MutableSet<UserTask>>().withDefault { mutableSetOf() }
+
     fun addTask(userId: Int, taskDescription: String, dueDate: Int, tags: List<String>): Int {
-        return 0
+        // Generate the task id
+        val taskId = (_userTasks[userId]?.size ?: 0) + 1
+
+        // Create a new task for the user
+        val newTask = Task(
+            id = taskId,
+            description = taskDescription,
+            dueDate = dueDate,
+            tags = tags,
+        )
+
+        // Add the task to the users tasks by id
+        _userTasks[userId]?.put(taskId, newTask)
+
+        // If it has tags add it to the tag index
+        if(tags.isNotEmpty()){
+            indexTaskByTags(userId, taskId, tags)
+        }
+
+        return taskId
     }
 
     fun getAllTasks(userId: Int): List<String> {
@@ -16,4 +38,20 @@ class TodoList {
     fun completeTask(userId: Int, taskId: Int) {
 
     }
+
+    private fun indexTaskByTags(userId: Int, taskId: Int, tags: List<String>) {
+        for(tag in tags) {
+            _tagIndex[tag]?.add(UserTask(userId, taskId))
+        }
+    }
+
+    private data class Task(
+        val id: Int = 0,
+        val description: String = "",
+        val dueDate: Int,
+        val isCompleted: Boolean = false,
+        val tags: List<String> = emptyList()
+    )
+
+    private data class UserTask(val userId: Int, val taskId: Int)
 }
