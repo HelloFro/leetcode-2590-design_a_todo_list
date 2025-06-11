fe3661a8cce21b1d8a72917bd09f421c2a0e8c2a
Fro Warner <hellofroenn@gmail.com>
2025-06-11 13:37:15 -0700

    simplify test setup and add new test for getTasksForTag

--- src/test/kotlin/TodoListTests.kt
+++ src/test/kotlin/TodoListTests.kt
@@ -11,13 +11,14 @@ class TodoListTests {
         var todoList: TodoList? = null
         val actual = mutableListOf<Any?>()
         for (i in test.given.functions.indices) {
-            when (test.given.functions[i]) {
+            val currentFunction = test.given.functions[i]
+            when (currentFunction) {
                 Functions.InitTodoList -> {
                     todoList = TodoList()
                     actual.add(null)
                 }
-                Functions.AddTask -> {
-                    val task = test.given.params[i] as Task
+                is Functions.AddTask -> {
+                    val task = currentFunction
                     val result = todoList!!.addTask(
                         task.userId,
                         task.description,
@@ -26,18 +27,18 @@ class TodoListTests {
                     )
                     actual.add(result)
                 }
-                Functions.GetAllTasks -> {
-                    val userId = test.given.params[i] as Int
+                is Functions.GetAllTasks -> {
+                    val userId = currentFunction.userId
                     val result = todoList!!.getAllTasks(userId)
                     actual.add(result)
                 }
-                Functions.GetTasksForTag -> {
-                    val userTaskByTag = test.given.params[i] as UserTaskByTag
+                is Functions.GetTasksForTag -> {
+                    val userTaskByTag = currentFunction
                     val result = todoList!!.getTasksForTag(userTaskByTag.userId, userTaskByTag.tag)
                     actual.add(result)
                 }
-                Functions.CompleteTask -> {
-                    val userTask = test.given.params[i] as UserTask
+                is Functions.CompleteTask -> {
+                    val userTask = currentFunction
                     todoList!!.completeTask(userTask.userId, userTask.taskId)
                     actual.add(null)
                 }
@@ -54,15 +55,16 @@ class TodoListTests {
                 name = "Create TodoList",
                 given = Given(
                     functions = listOf(Functions.InitTodoList),
-                    params = listOf<Any?>(null)
                 ),
                 expected = Expected(output = mutableListOf(null))
             ),
             Test(
                 name = "Create TodoList then addTask",
                 given = Given(
-                    functions = listOf(Functions.InitTodoList, Functions.AddTask),
-                    params = listOf<Any?>(null, Task(1, "Task1", 50, emptyList()))
+                    functions = listOf(
+                        Functions.InitTodoList,
+                        Functions.AddTask(1, "Task1", 50, emptyList())
+                    )
                 ),
                 expected = Expected(output = mutableListOf(null, 1))
             ),
@@ -71,13 +73,8 @@ class TodoListTests {
                 given = Given(
                     functions = listOf(
                         Functions.InitTodoList,
-                        Functions.AddTask,
-                        Functions.AddTask
-                    ),
-                    params = listOf<Any?>(
-                        null,
-                        Task(1, "Task1", 50, emptyList()),
-                        Task(1, "Task2", 100, listOf("P1"))
+                        Functions.AddTask(1, "Task1", 50, emptyList()),
+                        Functions.AddTask(1, "Task2", 100, listOf("P1"))
                     )
                 ),
                 expected = Expected(
@@ -91,20 +88,48 @@ class TodoListTests {
                 given = Given(
                     functions = listOf(
                         Functions.InitTodoList,
-                        Functions.AddTask,
-                        Functions.AddTask,
-                        Functions.GetAllTasks
+                        Functions.AddTask(1, "Task1", 50, emptyList()),
+                        Functions.AddTask(1, "Task2", 100, listOf("P1")),
+                        Functions.GetAllTasks(1)
                     ),
-                    params = listOf(
-                        null,
-                        Task(1, "Task1", 50, emptyList()),
-                        Task(1, "Task2", 100, listOf("P1")),
-                        1
-                    )
                 ),
                 expected = Expected(
                     output = mutableListOf(
                         null, 1, 2, listOf("Task1", "Task2")
+                    )
+                )
+            ),
+            Test(
+                name = "Create TodoList, 3x addTask, and getTasksForTag",
+                given = Given(
+                    functions = listOf(
+                        Functions.InitTodoList,
+                        Functions.AddTask(1, "Task1", 50, emptyList()),
+                        Functions.AddTask(1, "Task2", 100, listOf("P1")),
+                        Functions.AddTask(1, "Task3", 30, listOf("P1")),
+                        Functions.GetTasksForTag(1,"P1")
+                    ),
+                ),
+                expected = Expected(
+                    output = mutableListOf(
+                        null, 1, 2, 3, listOf("Task3", "Task2")
+                    )
+                )
+            ),
+            Test(
+                name = "Test getTasksForTag when different users have the same tag",
+                given = Given(
+                    functions = listOf(
+                        Functions.InitTodoList,
+                        Functions.AddTask(1, "Task1", 50, listOf("P1")),
+                        Functions.AddTask(2, "Task2", 100, listOf("P1")),
+                        Functions.AddTask(3, "Task3", 30, listOf("P1")),
+                        Functions.GetTasksForTag(2,"P1")
+                    )
+                ),
+                expected = Expected(
+                    output = mutableListOf(
+                        null, 1, 1, 1, listOf("Task2")
                     )
                 )
             ),
@@ -117,7 +142,7 @@ class TodoListTests {
         }
     }
 
-    data class Given(val functions: List<Functions>, val params: List<Any?>)
+    data class Given(val functions: List<Functions>)
 
     data class Expected(val output: MutableList<Any?>){
         override fun toString(): String {
@@ -127,20 +152,22 @@ class TodoListTests {
 
     sealed class Functions {
         data object InitTodoList : Functions()
-        data object AddTask : Functions()
-        data object GetAllTasks : Functions()
-        data object GetTasksForTag : Functions()
-        data object CompleteTask : Functions()
+        data class AddTask(
+            val userId: Int = 0,
+            val description: String = "",
+            val dueDate: Int,
+            val tags: List<String> = emptyList()
+        ) : Functions()
+        data class GetAllTasks(
+            val userId: Int = 0
+        ) : Functions()
+        data class GetTasksForTag(
+            val userId: Int,
+            val tag: String
+        ) : Functions()
+        data class CompleteTask(
+            val userId: Int,
+            val taskId: Int
+        ) : Functions()
     }
-
-    private data class Task(
-        val userId: Int = 0,
-        val description: String = "",
-        val dueDate: Int,
-        val tags: List<String> = emptyList()
-    )
-
-    private data class UserTask(val userId: Int, val taskId: Int)
-
-    private data class UserTaskByTag(val userId: Int, val tag: String)
 } 
